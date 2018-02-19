0b147108f365b82d4fd8bff519c65eeaeedf25a9
Phillip <phillipables1@gmail.com>
2018-02-19 08:09:53 -0500

    import chat.txt into main and train out bot to the conversation then while true get user input and let our bot respond

--- main.py
+++ main.py
@@ -2,3 +2,15 @@ from chatterbot.trainers import ListTrainer #method to train the chatbot
 from chatterbot import ChatBot #import the ChatBot
 
 bot = ChatBot('Text') #create the chatbot
+
+conv = open('chat.txt', 'r').readlines()
+
+bot.set_trainer(ListTrainer) # set the trainers
+
+bot.train(conv) # train the bot\
+
+while True:
+    request = input('You: ')
+    response = bot.get_response(request)
+
+    print('Bot: ', response)
