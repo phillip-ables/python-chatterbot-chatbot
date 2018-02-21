509afe43d19b7e452cbc517cd473cd535bbd2e6a
Phillip <phillipables1@gmail.com>
2018-02-20 20:52:10 -0500

    fixed bugs so it actually works now

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-from chatterbot.trainers import ListTrainer #method to train the chatbot
-from chatterbot import ChatBot #import the ChatBot
+from chatterbot.chatterbot import ChatBot
+from pep8 import readlines
+from chatterbot.trainers import ListTrainer  # method to train the chatbot
 
-bot = ChatBot('Text') #create the chatbot
+bot = ChatBot('Test')  # create the chatbot
 
 conv = open('chat.txt', 'r').readlines()
 
-bot.set_trainer(ListTrainer) # set the trainers
-
-bot.train(conv) # train the bot\
-
 while True:
+    bot.set_trainer(ListTrainer)  # set the trainers
+    bot.train(conv)  # train the bot
     request = input('You: ')
     response = bot.get_response(request)
-
     print('Bot: ', response)
+    
