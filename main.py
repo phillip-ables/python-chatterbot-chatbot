3ce3f7119763b7fb374933806b4fbc6eefa7c634
Phillip <phillipables1@gmail.com>
2018-02-20 21:10:53 -0500

    file directory and new txt file to put in with the other txt file

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from chatterbot.chatterbot import ChatBot
+from chatterbot.chatterbot import ChatBot  # import the chatbot
 from pep8 import readlines
 from chatterbot.trainers import ListTrainer  # method to train the chatbot
 
@@ -6,9 +6,11 @@ bot = ChatBot('Test')  # create the chatbot
 
 conv = open('chat.txt', 'r').readlines()
 
+
+bot.set_trainer(ListTrainer)  # set the trainers
+bot.train(conv)  # train the bot
+
 while True:
-    bot.set_trainer(ListTrainer)  # set the trainers
-    bot.train(conv)  # train the bot
     request = input('You: ')
     response = bot.get_response(request)
     print('Bot: ', response)
